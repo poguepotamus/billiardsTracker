4d1cd3996b2b98f778242bb50130b6ad60ef0f2f
poguepotamus <matthewpogue606@gmail.com>
2020-10-04 22:54:35 -0500

    Inital code file to create the module folder

---
+++ billiardsTracker/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/env python38
+
+# PySide2 imports
+from PySide2 import QtCore, QtWidgets, QtGui QtUiTools
+
+# Ugh, I don't want to start all this tonight. I'll make a UI file and shit some other time.
+
+if __name__ == '__main__':
+	print('This is where the window would go lmaoooo')
